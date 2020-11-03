787fedae1379e50a255355c375176580056a5ff4
Niks139 <p.niks007@gmail.com>
2020-11-03 01:42:36 -0500

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,21 @@
-import { FiberManualRecord } from '@material-ui/icons'
+
 import React from 'react'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
 import '../css/Sidebar.css'
 import SidebarOption from './SidebarOption';
 import '../css/SidebarOption.css';
+
 import CommentIcon from '@material-ui/icons/Comment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
@@ -21,13 +31,22 @@ function Sidebar() {
                 </div>
                 <CreateIcon />              
             </div>
-            <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
-            <SidebarOption Icon={} title="Threads"/>
-            <SidebarOption Icon={} title="Threads"/>
-            <SidebarOption Icon={} title="Threads"/>
-            <SidebarOption Icon={} title="Threads"/>
-            <SidebarOption Icon={} title="Threads"/>
+            <SidebarOption Icon={CommentIcon} title="Threads"/>
+            <SidebarOption Icon={InboxIcon} title="Mentions & reactions"/>
+            <SidebarOption Icon={DraftsIcon} title="Saved items"/>
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser"/>
+            <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
+            <SidebarOption Icon={AppsIcon} title="Apps"/>
+            <SidebarOption Icon={FileCopyIcon} title="File browser"/>
+            <SidebarOption Icon={ExpandLessIcon} title="Show less"/>
+            <SidebarOption title="YouTube"/>
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+            <hr />
+            <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
 
+            {/*Connect to db and display all the channels */}
+            {/* <SidebarOptions .../> */}
             
         </div>
     )
